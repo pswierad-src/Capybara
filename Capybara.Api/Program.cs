247e11caeb3a0ce175b882f1b335b89fa2ec5b9e
pswierad <pswierad@mobitouch.net>
2021-11-23 13:21:02 +0100

    Added in memory database

--- Capybara.Api/Program.cs
+++ Capybara.Api/Program.cs
@@ -6,14 +6,11 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
